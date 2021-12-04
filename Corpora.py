1abf1a25f8d6c3f815d43965c1f9c39438ada960
Katie Collins <kcollins@Katies-iMac.local>
2021-12-04 12:06:16 +0000

    first pass at dataloader

--- Corpora.py
+++ Corpora.py
@@ -3,7 +3,7 @@ from nltk.stem.porter import PorterStemmer
 import numpy as np
 
 class MovieReviewCorpus():
-    def __init__(self,stemming,pos):
+    def __init__(self,stemming,pos,use_imdb=False):
         """
         initialisation of movie review corpus.
 
@@ -26,8 +26,17 @@ class MovieReviewCorpus():
         self.stemmer=PorterStemmer() if stemming else None
         # part-of-speech tags
         self.pos=pos
+        
+        # which dataset to pull from (added flag)
+        self.use_imdb = use_imdb
+        if use_imdb: 
+            self.data_dir = f"data/aclImdb/"
+        else: 
+            self.data_dir = f"data/reviews/"
+        
+        
         # import movie reviews
-        self.get_reviews()
+        self.get_reviews(self.data_dir)
 
     def get_reviews(self, data_dir = f"data/reviews/"):
         """
@@ -48,9 +57,15 @@ class MovieReviewCorpus():
         3. store reviews in self.folds. self.folds is a dictionary with the format: self.folds[fold_number] where fold_number is an int 0-9.
            you can get the fold number from the review file name.
         """
-
+        
+        # maintain lists that we want info from
+        train_info = []
+        test_info = []
+        cv_info = {} # round robin
+        cv_info_conseq = {}
+        
         # convert a single review into (token, pos-tag format)
-        def get_single_review(fpth):
+        def get_single_review_wPOS(fpth):
             # read in data from a single file (each file = a single review)
             # help reading in data from: https://www.pythontutorial.net/python-basics/python-read-text-file/
             with open(fpth) as f:
@@ -73,44 +88,93 @@ class MovieReviewCorpus():
                 parsed_review_data.append(data_obj)#(token, pos_tag))
 
             return parsed_review_data
+       
+        # convert a single review into just token list
+        def get_single_review(fpth):
+            # read in data from a single file (each file = a single review)
+            # help reading in data from: https://www.pythontutorial.net/python-basics/python-read-text-file/
+            with open(fpth) as f:
+                # NOTE: assumes we want to split by space -- discuss!!
+                # e.g., "kick the bucket" diff meaning than individ words, but sep by spaces
+                full_review_data = f.readlines()[0].split(" ") 
 
-        # define sentiment classes (note: could be changed in the future)
-        sentiment_classes = ["POS", "NEG"]
+            # todo (for future): could optionally ignore punctuation!!
+            parsed_review_data = []
+            for token in full_review_data:
+                token = token.lower() # OPTIONAL!!!! discuss!!
+                if self.stemming: 
+                    token = self.stemmer.stem(token)
+                data_obj = token#(token, pos_tag)
+                parsed_review_data.append(data_obj)#(token, pos_tag))
 
-        # maintain lists that we want info from
-        train_info = []
-        test_info = []
-        cv_info = {} # round robin
-        cv_info_conseq = {}
+            return parsed_review_data
+        
+        if not self.use_imdb:
 
-        for sent_class in sentiment_classes:
-            sent_dir = f"{data_dir}{sent_class}/" # "sent" = "sentiment"
-            all_reviews = [rev for rev in os.listdir(sent_dir) if rev[-4:] == ".tag"]
+            # define sentiment classes (note: could be changed in the future)
+            sentiment_classes = ["POS", "NEG"]
 
-            # process each review and put in associated train/test based on file number
-            # (also determines fold)
-            for review_idx, review_file_name in enumerate(all_reviews):
+            for sent_class in sentiment_classes:
+                sent_dir = f"{data_dir}{sent_class}/" # "sent" = "sentiment"
+                all_reviews = [rev for rev in os.listdir(sent_dir) if rev[-4:] == ".tag"]
 
-                fold_num = int(review_file_name[3]) # all start w/ cv
-                parsed_review_data = get_single_review(f"{sent_dir}{review_file_name}")
-                review_metadata = [sent_class, parsed_review_data]
+                # process each review and put in associated train/test based on file number
+                # (also determines fold)
+                for review_idx, review_file_name in enumerate(all_reviews):
 
-                if fold_num == 9:
-                    test_info.append(review_metadata)
-                else:
-                    train_info.append(review_metadata)
+                    fold_num = int(review_file_name[3]) # all start w/ cv
+                    parsed_review_data = get_single_review_wPOS(f"{sent_dir}{review_file_name}")
+                    review_metadata = [sent_class, parsed_review_data]
 
-                # TODO: update w/ round-robin splitting
-                fold_num = review_idx % 10 # b/c mod-10
-                if fold_num not in cv_info:
-                    cv_info[fold_num] = [review_metadata]
-                else:
-                    cv_info[fold_num].append(review_metadata)
+                    if fold_num == 9:
+                        test_info.append(review_metadata)
+                    else:
+                        train_info.append(review_metadata)
+
+                    # TODO: update w/ round-robin splitting
+                    fold_num = review_idx % 10 # b/c mod-10
+                    if fold_num not in cv_info:
+                        cv_info[fold_num] = [review_metadata]
+                    else:
+                        cv_info[fold_num].append(review_metadata)
 
 
-                # CONSECUTIVE SPLITTING
-                if fold_num not in cv_info_conseq: cv_info_conseq[fold_num] = [review_metadata]
-                else: cv_info_conseq[fold_num].append(review_metadata)
+                    # CONSECUTIVE SPLITTING
+                    if fold_num not in cv_info_conseq: cv_info_conseq[fold_num] = [review_metadata]
+                    else: cv_info_conseq[fold_num].append(review_metadata)
+        else: 
+            # parse reviews from imbd data format
+            supra_folders = ["train", "test"]
+            sub_folders = ["pos", "neg"]
+            for split in supra_folders: # corresponds to train/test main split
+                for sent_class in sub_folders: # corresponds to labeled sentiment
+                    sent_dir = f"{data_dir}{split}/{sent_class}/" 
+                    all_reviews = [rev for rev in os.listdir(sent_dir) if rev[-4:] == ".txt"]   
+                    # process each review and put in associated train/test based on file number
+                    # (also determines fold)
+                    for review_idx, review_file_name in enumerate(all_reviews):
+
+                        orig_fold_num = int(review_file_name.split("_")[1].split(".")[0]) # include split at end
+                        parsed_review_data = get_single_review(f"{sent_dir}{review_file_name}")
+                        sent_class = sent_class.upper() # for consistency with other dataset -- use all caps w/ labels
+                        review_metadata = [sent_class, parsed_review_data]
+
+                        if split == "test":
+                            test_info.append(review_metadata)
+                        else:
+                            train_info.append(review_metadata)
+
+                        # round robin
+                        fold_num = review_idx % 10 # b/c mod-10
+                        if fold_num not in cv_info:
+                            cv_info[fold_num] = [review_metadata]
+                        else:
+                            cv_info[fold_num].append(review_metadata)
+
+
+                        # CONSECUTIVE SPLITTING
+                        if orig_fold_num not in cv_info_conseq: cv_info_conseq[orig_fold_num] = [review_metadata]
+                        else: cv_info_conseq[orig_fold_num].append(review_metadata)
 
         # set class attributes using curated metadata
         self.train = train_info
